5ed6ec05a0062e9f91d14be49929e6bf236a7ea8
AJ Carl Dy <ajcarl.dy@gmail.com>
2026-06-09 17:32:06 +0800

    Fix test_evaluate_model

--- tests/test_evaluate_model.py
+++ tests/test_evaluate_model.py
@@ -5,13 +5,13 @@ from dsa.constants import ROOT
 from dsa.evaluate_model import evaluate
 from dsa.utils import load_json
 
-@pytest.mark.skip(reason="Broken test")
 def test_evaluate_model():
-    ref_path = ROOT / "tests/data/report_chatgpt3.json"
-    gt_path = ROOT / "tests/data/ground_truth.json"
-    pred_path = ROOT / "tests/data/chatgpt3.json"
+    ref_path = ROOT / "tests/data/evaluate_model_ref.json"
+    gt_path = ROOT / "tests/data/evaluate_model_gt.json"
+    pred_path = ROOT / "tests/data/evaluate_model_pred.json"
 
     ref = load_json(ref_path)
+    del ref["info"]
     res = evaluate(
         gt_json_path=gt_path,
         pred_json_path=pred_path,
@@ -19,4 +19,5 @@ def test_evaluate_model():
         labels=["Figure", "Table"],
         output_path=None,
     )
+    del res["info"]
     assert json.dumps(ref) == json.dumps(res)
